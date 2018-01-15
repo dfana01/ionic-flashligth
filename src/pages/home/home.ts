8e577be14aa8c34307a5cf16774d9baa56bdf30b
Dante Faña <dfana@dfb.com.do>
2018-01-14 22:25:27 -0400

    flashlight funcionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Platform } from 'ionic-angular'
+import { Flashlight } from '@ionic-native/flashlight';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,31 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  isOn: boolean;
 
+  constructor(private flashlight: Flashlight, 
+    private platform: Platform) {
+    this.platform.ready().then(() => {
+      this.updateFlashlightStatus();
+    });
   }
 
+  switchOn(){
+    this.flashlight.switchOn();
+    this.updateFlashlightStatus();
+  }
+
+  switchOff(){
+    this.flashlight.switchOff();
+    this.updateFlashlightStatus();
+  }
+
+  toggle(){
+    this.flashlight.toggle();
+    this.updateFlashlightStatus();
+  }
+
+  updateFlashlightStatus(){
+    this.isOn = this.flashlight.isSwitchedOn();
+  }
 }
